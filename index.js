29e59f21df2056e27113e904bbaeafa1e11328aa
Hemachandru <2006hemachandru@gmail.com>
2026-07-03 11:17:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 // Complete the function using object destructuring.
 
+let studentDetails = [
+    {name: "ahalya", dept: "bca", age: 19},
+    {name: "hema", dept: "mca", age: 21},
+    {name: "chandru", dept:"bcam", age: 20}
+];
+
 function getStudentDetails(students) {
-    // Write your code here
+    const {name, dept, age} = studentDetails;
+    
 }
 
 module.exports = getStudentDetails;
